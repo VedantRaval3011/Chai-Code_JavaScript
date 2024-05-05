2e24ab11ac13dd64c3987232bb389827b7f0ee79
VedantRaval3011 <121101897+VedantRaval3011@users.noreply.github.com>
2024-05-05 09:17:18 +0000

    commit array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,27 @@
+//array
+
+const myArr = [5, 1, 2, 3, 4, 5, true, "vedant"]
+
+const myHeros = ["Shaktiman","nagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+
+// console.log(myArr[0]);
+
+
+//Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join();
+
+console.log(myArr);
+console.log(newArr);
